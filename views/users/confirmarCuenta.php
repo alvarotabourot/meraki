8aaddab4f545c3221685b386e42c1403b59d7375
alvarotabourot <alvarotabourot141@gmail.com>
2024-06-20 01:52:17 +0200

    Corrección css mensajes verificacion

--- views/users/confirmarCuenta.php
+++ views/users/confirmarCuenta.php
@@ -5,13 +5,15 @@ incluirTemplate('header');
 
 <?php include_once __DIR__."/../../includes/alertas.php"; ?>
 
-
-<div class="verificacion">
-    <p>Hola <?= $usuario->nombreUsuario ?>. Su cuenta <?= $usuario->email ?> ha sido confirmada.</p>
-    <p>Gracias por confirmar su cuenta.</p>
-    <p>Para nosotros es importante asegurarnos de que nuestros usuarios son personales reales.</p>
-    <p>Ya puede iniciar sesión sin problema pulsando aqui: <a href="<?= $_ENV['BASE_URL']?>login">Iniciar sesion</a></p>
+<div class="centrar-verificacion">
+    <div class="verificacion">
+        <p>Hola <?= $usuario->nombreUsuario ?>. Su cuenta <?= $usuario->email ?> ha sido confirmada.</p>
+        <p>Gracias por confirmar su cuenta.</p>
+        <p>Para nosotros es importante asegurarnos de que nuestros usuarios son personales reales.</p>
+        <p>Ya puede iniciar sesión sin problema pulsando aqui: <a href="<?= $_ENV['BASE_URL']?>login">Iniciar sesion</a></p>
+    </div>
 </div>
+
 
 <?php 
 
--- views/users/mensaje.php
+++ views/users/mensaje.php
@@ -2,11 +2,14 @@
 include_once __DIR__."/../../includes/funciones.php";
 incluirTemplate('header');
 ?>
-<div class="verificacion">
-    <p>Su cuenta ha sido registrada correctamente.</p>
-    <p>Ahora necesitamos que verifique la cuenta, para ello le hemos enviado unas instrucciones al correo con el cual se registró.</p>
-    <p><strong>OJO!!</strong> Si no le aparece en su bandeja revise las demás carpetas.</p>
-    <p>¡¡Gracias y bienvenido a la familia de MERAKI!!</p>
+
+<div class="centrar-verificacion">
+    <div class="verificacion">
+        <p>Su cuenta ha sido registrada correctamente.</p>
+        <p>Ahora necesitamos que verifique la cuenta, para ello le hemos enviado unas instrucciones al correo con el cual se registró.</p>
+        <p><strong>OJO!!</strong> Si no le aparece en su bandeja revise las demás carpetas.</p>
+        <p>¡¡Gracias y bienvenido a la familia de MERAKI!!</p>
+    </div>
 </div>
 
 
